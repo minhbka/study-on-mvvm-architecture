4ec34b048fcdd7935e5134e15a889723918a0e9a
Phan Minh <minhpv@kormedi.com>
2020-03-08 19:03:39 +0900

    project setup

---
+++ app/src/main/java/com/minhbka/studyonmvvmarchitecture/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.minhbka.studyonmvvmarchitecture.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minhbka.studyonmvvmarchitecture.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/minhbka/studyonmvvmarchitecture/ui/auth/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.minhbka.studyonmvvmarchitecture.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minhbka.studyonmvvmarchitecture.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
